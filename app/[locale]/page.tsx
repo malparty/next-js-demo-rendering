c3ab1ef470947487d2128d6c8d96813d88e2e570
Xavier MALPARTY <xavier@nimblehq.co>
2024-05-21 10:08:52 +0700

    Make the locale page static

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,14 @@ interface DynamicPageProps {
   params: {locale: string}
 }
 
+const LOCALES = ['en', 'th', 'vn'];
+
+export async function generateStaticParams() {
+  return LOCALES.map((lang) => ({
+    locale: lang,
+  }))
+}
+
 export default function DynamicPage({ params }: DynamicPageProps) {
   return (
     <div>
