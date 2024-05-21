07e2c9dad532f03fb574757789636b63b4dc073f
Xavier MALPARTY <xavier@nimblehq.co>
2024-05-21 11:09:20 +0700

    Add time link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { ClientButton } from '@/components/ClientButton';
+import { Time } from '@/components/Time';
 import { headers } from 'next/headers';
+import Link from 'next/link';
 
 export default async function Home() {
   const headersData = headers()
+  const seconds = new Date().getSeconds();
+
   return (
     <main className="m-4 p-4 border-2 border-blue-600">
       <div className="text-blue-600">
@@ -11,10 +15,14 @@ export default async function Home() {
 
       <div>
         <ClientButton>
-          <div className='text-blue-500'>
-            Server Side content: {headersData.get('host')}
+          <div className='text-blue-500 border-2 border-blue-500 m-2 p-2 w-max'>
+            Server Side children: {headersData.get('host')}
+            <Time />
           </div>
         </ClientButton>
+        <div className="text-blue-500 text-sm">
+          <Link href={`/?seconds=${seconds}`}>Nav link to {seconds}s</Link>
+        </div>
       </div>
     </main>
   );
---
+++ components/Time.tsx
@@ -0,0 +1,1 @@
+export const Time = () => (<div>{new Date().getSeconds()}s.</div>)
